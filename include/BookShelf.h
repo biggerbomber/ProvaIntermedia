ad83578251e5cbc0e82fd40cd15cb6d0e863c914
ALBERTO BARBATO <ab2073961@adt020.adt.unipd.it>
2023-11-20 12:04:50 +0100

    header BookShelf

---
+++ include/BookShelf.h
@@ -0,0 +1,19 @@
+#ifndef _BOOKSHELF_H
+#define _BOOKSHELF_H
+
+#include "include/MyVector.h"
+#include "include/Book.h"
+
+class BookShelf{
+	
+public:
+	BookShelf(int);
+	
+	void push_back(Book);
+	
+	Book pop_back();
+private:
+	MyVector m_books;
+}
+
+#endif
