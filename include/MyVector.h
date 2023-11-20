fda4cada36e329ac857dcc2af6f478c04d55cc9b
LEONARDO CASUCCI <lc2073980@adt021.adt.unipd.it>
2023-11-20 11:48:30 +0100

    header di MyVector

---
+++ include/MyVector.h
@@ -0,0 +1,39 @@
+#ifndef _MYVECTOR_H
+#define _MYVECTOR_H
+
+#include <iostream>
+
+class MyVector{
+public:
+	MyVector();
+	explicit MyVector(int);
+	MyVector(const MyVector&);
+	MyVector(initializer_list<double>);
+	~MyVector();
+
+	
+	double operator[](int) const;
+	double& operator[](int);
+	
+	double at(int) const;
+	double& at(int);
+	
+	void reserve(int);
+
+	void push_back(double);
+	double pop_back();
+	
+	int size() {return m_size;}//inline
+	double* get_pointer(){return m_data;}
+  
+	
+private:
+	double* m_data {nullptr};
+	int m_size {0};
+	int m_capacity {0};
+	
+};
+
+std::ostream& operator<<(std::ostream&, MyVector&);
+
+#endif
