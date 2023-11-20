a68f220940199c14720781b7501cf7dab0988a5b
MATTIA PINTON <mp2066703@adt022.adt.unipd.it>
2023-11-20 12:17:13 +0100

    header date e book

---
+++ include/Book.h
@@ -0,0 +1,18 @@
+#ifndef _BOOK_H
+#define _BOOK_H
+
+#include <iostream>
+#include <string>
+
+class Book{
+	private:
+	std::string m_ISBN;
+	std::string m_titolo;
+	std::string m_nomeAutore;
+	std::string m_cognomeAutore;
+	Date m_data;
+	
+	public:
+};
+
+#endif
