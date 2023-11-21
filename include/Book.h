998c6ee81853db33b23197ce8d26402092d441ad
biggerbomber <76782912+biggerbomber@users.noreply.github.com>
2023-11-21 19:37:33 +0100

    Implementazione e modifica interfaccia Book, Da finire

--- include/Book.h
+++ include/Book.h
@@ -5,59 +5,17 @@
 #include <string>
 #include "Date.h"
 
-/*
-1.
-Progettare e implementare la classe Book che rappresenta un libro. Tale classe ha membri per
-contenere:
-a.
-ISBN;
-b.
-titolo;
-c.
-nome e cognome autore (due membri diversi) – è previsto un solo autore per libro;
-d.
-data di copyright – la data è rappresentata usando una apposita classe Date simile a
-quella sviluppata a lezione;
-e.
-stato in prestito / disponibile.
-Il codice ISBN è rappresentato mediante una std::string il cui unico requisito è quello di avere
-lunghezza 13.
-La classe Book deve essere dotata di:
-a.
-funzioni membro che ritornano i vari dati membro;
-b.
-costruttori;
-c.
-funzioni per registrare il prestito e la restituzione;
-d.
-appositi strumenti (eventualmente implementati nella classe dedicata) per la validazione
-della data;
-e.
-operator== e operator!= che confrontano due Book basandosi sul codice ISBN;
-f.
-operator<< che stampa titolo, autore, ISBN e la data di copyright su righe separate in
-output.
-La classe Book deve permette di eseguire la seguente operazione:
-a. Book
-mybook(“David”,
-“Foster
-Wallace”,
-“Una
-cosa
-divertente
-che
-non farò mai più”, “887-521-837-4”);
-*/
+
 
 class Book{
-	public:
+public:
 	
 	//COSTRUTTORI
-	Book(); //default
-	Book(std::string); //ibsn
-	Book(std::string, std::string); //titolo, ibsn
-	Book(std::string,std::string,std::string,std::string);//nome, cognome, titolo, ibsn
-	Book(std::string,std::string,std::string,std::string, Date);//nome, cognome, titolo, ibsn, copyright
+	//Book(); //default
+	//Book(const std::string&); //ibsn
+	//Book(const std::string&, const std::string&); //titolo, ibsn
+	Book(const std::string&, const std::string&, const std::string&, const std::string&);//nome, cognome, titolo, ibsn
+	Book(const std::string&, const std::string&, const std::string&, const std::string&, Date);//nome, cognome, titolo, ibsn, copyright
 	
 	// MV/CPY CONSTR
 	Book(const Book&); //cpy
@@ -69,7 +27,7 @@ class Book{
 	//OVERLOAD DEGLI OPERATORI
 	
 	Book& operator=(const Book&); //cpy
-	Book& operator=(const Book&) const;  //cpy
+	//Book& operator=(const Book&) const;  //cpy ERRORE
 	
 	Book& operator=(Book&&); //mv
 	
@@ -78,24 +36,38 @@ class Book{
 	//FUNZIONI MEMBRO GENERICHE 
 	
 	void presta();
-	void restituisci();
+	void restituito();
 	
 	//FUNZIONI DI ISPEZIONE
 	
-	std::string ibsn();
-	std::string titolo();
-	std::string nomeAutore();
-	std::string cognomeAutore();
-	bool disponibile();
-	Date copyright();
+	std::string ibsn() { return m_ISBN; };
+	std::string titolo() { return m_titolo; };
+	std::string nomeAutore() { return m_nomeAutore; };
+	std::string cognomeAutore() { return m_cognomeAutore; };
+	bool disponibile() { return m_disponibile; };
+	Date copyright() { return m_dataCopyright; };
 	
-	private:
-	std::string m_ISBN {"0000000000000"};
-	std::string m_titolo {"nessun titolo"};
-	std::string m_nomeAutore {"Nessuno"};
-	std::string m_cognomeAutore {"Niente"};
-	bool m_disponibile {true};
-	Date m_copyright;
+	//Funzioni di set.
+
+	void setIbsn(const std::string& isbn);
+	void setTitolo(const std::string& titolo) { m_titolo=titolo; };
+	void setNomeAutore(const std::string& nomeAutore) { m_nomeAutore = nomeAutore; };
+	void setCognomeAutore(const std::string& cognomeAutore) { m_cognomeAutore=cognomeAutore; };
+	void setDisponibile(bool b) { m_disponibile = b; };
+	void setCopyright(Date dataCopyright) { m_dataCopyright = dataCopyright; };
+
+
+
+
+private:
+	std::string m_ISBN {""};
+	std::string m_titolo {""};
+	std::string m_nomeAutore {""};
+	std::string m_cognomeAutore {""};
+	bool m_disponibile {false};
+	Date m_dataCopyright;
+
+	bool isIsbnValid(const std::string&);
 
 };
 
---
+++ src/Book.cpp
@@ -0,0 +1,84 @@
+#include "../include/Book.h"
+Book::Book(const std::string& nomeAutore, 
+		const std::string& cognomeAutore, 
+		const std::string& titolo, 
+		const std::string& isbn) {
+
+	if (!isIsbnValid(isbn)) {
+		throw std::invalid_argument("ISBN deve avere 13 caratteri");
+	}
+	m_ISBN = isbn;
+	m_nomeAutore = nomeAutore;
+	m_cognomeAutore = cognomeAutore;
+	m_titolo = titolo;
+	m_disponibile = true;
+}
+
+bool Book::isIsbnValid(const std::string& isbn) {
+	return isbn.size() == 13;
+}
+Book::Book(const std::string& nomeAutore,
+	const std::string& cognomeAutore,
+	const std::string& titolo,
+	const std::string& isbn,
+	Date dataCopyright ) {
+
+	if (!isIsbnValid(isbn)) {
+		throw std::invalid_argument("ISBN deve avere 13 caratteri");
+	}
+	m_ISBN = isbn;
+	m_nomeAutore = nomeAutore;
+	m_cognomeAutore = cognomeAutore;
+	m_titolo = titolo;
+	m_dataCopyright = dataCopyright;
+	m_disponibile = true;
+}
+
+Book::Book(const Book& other) {
+	m_ISBN = other.m_ISBN;
+	m_nomeAutore = other.m_nomeAutore;
+	m_cognomeAutore = other.m_cognomeAutore;
+	m_disponibile = other.m_disponibile;
+	m_dataCopyright = other.m_dataCopyright;
+}
+Book::Book(Book&& other) {
+	m_ISBN = std::move(other.m_ISBN);
+	m_nomeAutore = std::move(other.m_nomeAutore);
+	m_cognomeAutore = std::move(other.m_cognomeAutore);
+	m_disponibile = other.m_disponibile;
+	m_dataCopyright = other.m_dataCopyright;
+}
+Book& Book::operator=(const Book& other) {
+	m_ISBN = other.m_ISBN;
+	m_nomeAutore = other.m_nomeAutore;
+	m_cognomeAutore = other.m_cognomeAutore;
+	m_disponibile = other.m_disponibile;
+	m_dataCopyright = other.m_dataCopyright;
+}
+Book& Book::operator=(Book&& other) {
+	m_ISBN = std::move(other.m_ISBN);
+	m_nomeAutore = std::move(other.m_nomeAutore);
+	m_cognomeAutore = std::move(other.m_cognomeAutore);
+	m_disponibile = other.m_disponibile;
+	m_dataCopyright = other.m_dataCopyright;
+}
+
+void Book::presta(){
+	if (!m_disponibile) {
+		throw std::invalid_argument("Il libro non è disponibile!");
+	}
+	m_disponibile = false;
+}
+void Book::restituito() {
+	if (m_disponibile) {
+		throw std::invalid_argument("Il libro non è stato prestato!");
+	}
+	m_disponibile = false;
+}
+
+void Book::setIbsn(const std::string& isbn){
+	if (!isIsbnValid(isbn)) {
+		throw std::invalid_argument("ISBN deve avere 13 caratteri");
+	}
+	m_ISBN = isbn;
+}
